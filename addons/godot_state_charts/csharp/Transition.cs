b4c8cabf702a68b87445a289e6d45b5cb02e3695
Jan Thomä <janthomae@janthomae.de>
2023-11-26 12:02:26 +0100

    chore: add new transition signal name

---
+++ addons/godot_state_charts/csharp/Transition.cs
@@ -0,0 +1,19 @@
+
+namespace GodotStateCharts
+{
+
+    /// <summary>
+    /// A transition between two states. This class only exists to make the 
+    /// signal names available in C#. It is not intended to be instantiated
+    /// or otherwise used.
+    /// </summary>
+    public class Transition {
+        public class SignalName : Godot.Node.SignalName
+        {
+            /// <summary>
+            /// Called when the transition is taken.
+            /// </summary>
+            public static readonly StringName Taken = "taken";
+        }
+    }
+}
