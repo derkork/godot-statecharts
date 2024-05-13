636612e4b384e93a326098f4eea6352beece4eec
Jan Thomä <janthomae@janthomae.de>
2024-05-13 19:22:30 +0200

    chore: cleanup

--- addons/godot_state_charts/utilities/debugger_history.gd
+++ addons/godot_state_charts/utilities/debugger_history.gd
@@ -16,43 +16,43 @@ func _init(maximum_lines:int = 500):
 
 ## Sets the maximum number of lines to store in the history.
 ## This will clear the history.
-func set_maximum_lines(maximum_lines:int):
+func set_maximum_lines(maximum_lines:int) -> void:
 	_buffer.set_maximum_lines(maximum_lines)
 
 
 ## Adds an item to the history list.
-func add_history_entry(frame:int, text:String):
+func add_history_entry(frame:int, text:String) -> void:
 	_buffer.append("[%s]: %s \n" % [frame, text])
 	_dirty = true
 
 
 ## Adds a transition to the history list.
-func add_transition(frame:int, name:String, from:String, to:String):
+func add_transition(frame:int, name:String, from:String, to:String) -> void:
 	add_history_entry(frame, "[»] Transition: %s from %s to %s" % [name, from, to])
 
 
 ## Adds an event to the history list.
-func add_event(frame:int, event:StringName):
+func add_event(frame:int, event:StringName) -> void:
 	add_history_entry(frame, "[!] Event received: %s" % event)
 
 
 ## Adds a state entered event to the history list.
-func add_state_entered(frame:int, name:StringName):
+func add_state_entered(frame:int, name:StringName) -> void:
 	add_history_entry(frame, "[>] Enter: %s" % name)
 
 
 ## Adds a state exited event to the history list.
-func add_state_exited(frame:int, name:StringName):
+func add_state_exited(frame:int, name:StringName) -> void:
 	add_history_entry(frame, "[<] Exit: %s" % name)
 
 
 ## Clears the history.
-func clear():
+func clear() -> void:
 	_buffer.clear()
 	_dirty = true
 
 
 ## Returns the full history as a string.
-func get_history_text():
+func get_history_text() -> String:
 	_dirty = false
 	return _buffer.join()
