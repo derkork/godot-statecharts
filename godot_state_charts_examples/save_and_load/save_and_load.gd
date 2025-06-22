5c44076791b6ac23188041d5c1a2adac25e9edb7
Jan Thomä <janthomae@janthomae.de>
2025-06-22 14:52:24 +0200

    refactor: simplify code of the example, add comments

--- godot_state_charts_examples/save_and_load/save_and_load.gd
+++ godot_state_charts_examples/save_and_load/save_and_load.gd
@@ -1,35 +1,45 @@
 extends Node2D
 
+## Path where we save our "game". This saved resource
+## contains the state of the state chart itself and
+## any other game specific state we want to save.
+const SAVE_PATH: StringName = "user://save_resource.tres"
 
-@export var chart : StateChart = null
-@export var box_nodes: Array[ColorBox] = []
+@onready var _state_chart: StateChart = %StateChart
+
+var _boxes: Array[ColorBox]
 
 
+func _ready():
+	# grab all color boxes
+	_boxes.assign(get_tree().get_nodes_in_group("colorbox"))
+	# send an event whenever a box requests a color change
+	for box in _boxes:
+		box.color_change_requested.connect(func(): _state_chart.send_event(box.box_id + "-click"))
+
+
+## Called when the "Save" button is pressed.
 func save_state() -> void:
-	var path = "user://save_resource.tres"
 	var save_resource: SaveResource = SaveResource.new()
-	save_resource.state_chart = StateChartSerializer.serialize(chart)
-	for box in box_nodes:
+	save_resource.state_chart = StateChartSerializer.serialize(_state_chart)
+	for box in _boxes:
 		save_resource.boxes.append(box.save_state())
-	ResourceSaver.save(save_resource, path)
 
+	ResourceSaver.save(save_resource, SAVE_PATH)
+
+## Called when the "Load" button is pressed.
 func load_state() -> void:
-	var path = "user://save_resource.tres"
-	var save_resource: SaveResource = ResourceLoader.load(path, "SaveResource")
-	StateChartSerializer.deserialize(save_resource.state_chart, chart)
+	var save_resource: SaveResource = ResourceLoader.load(SAVE_PATH)
+	## restore the state of the state chart
+	StateChartSerializer.deserialize(save_resource.state_chart, _state_chart)
 
-	var box_node_hash : Dictionary = {}
-	for node in box_nodes:
-		box_node_hash[node.box_id] = node
+	# make a lookup table for the boxes
+	var box_lut: Dictionary = {}
+	for node in _boxes:
+		box_lut[node.box_id] = node
 
+	# load the state of each box
 	for box_resource in save_resource.boxes:
-		var color_box: ColorBox = box_node_hash[box_resource.box_id]
+		var color_box: ColorBox = box_lut[box_resource.box_id]
 		color_box.load_state(box_resource)
 
-
-func _on_save_pressed() -> void:
-	save_state()
-
-
-func _on_load_pressed() -> void:
-	load_state()
