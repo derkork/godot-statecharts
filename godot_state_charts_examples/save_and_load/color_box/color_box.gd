dc51e12227380e4ea606b0db8eb7a8dfddb84eb5
Jan Thomä <janthomae@janthomae.de>
2025-06-22 14:52:24 +0200

    refactor: simplify code of the example, add comments

--- godot_state_charts_examples/save_and_load/color_box/color_box.gd
+++ godot_state_charts_examples/save_and_load/color_box/color_box.gd
@@ -1,89 +1,70 @@
-class_name ColorBox extends Container
+class_name ColorBox
+extends Container
 
-var colors: Array[Color] = [Color.RED, Color.GREEN, Color.BLUE]
+## The three colors that this box can have.
+const COLORS: Array[Color] = [Color.DARK_RED, Color.FOREST_GREEN, Color.CORNFLOWER_BLUE]
+## Emitted when someone presses the button
+signal color_change_requested()
+## The ID of this box. Used for saving.
+@export var box_id: StringName
 
-@export var color: Color = colors[0]
-@export var box_id: String = "box-1"
-@export var chart : StateChart = null
-@export var compound_state: CompoundState = null
-@export var red_state: StateChartState = null
-@export var green_state: StateChartState = null
-@export var blue_state: StateChartState = null
+## The color rect for the background color
+@onready var _color_rect: ColorRect = %ColorRect
+## Label showing the time we're in the current state
+@onready var _time_in_state_label: Label = %TimeInStateLabel
+## Label showing the pending transition time
+@onready var _transition_time_label: Label = %TransitionTimeLabel
 
 var _time_in_state: float = 0.0
-var _label: Label = null
-var _timer: Label = null
-var _transition_timer: Label = null
+var _current_color_index: int = 0
 
 
-func _ready() -> void:
-	_label = find_child("ID")
-	_label.text = box_id
+func _ready():
+	# emit the signal when the button is pressed
+	%Button.pressed.connect(func(): color_change_requested.emit())
+	# set the id
+	%IdLabel.text = box_id
+	# and initialize the color
+	_switch_to(0)
 
-	_timer = find_child("Timer")
-	_timer.text = str(_time_in_state)
+## Called when this box should switch to red.
+func switch_to_red():
+	_switch_to(0)
 
-	_transition_timer = find_child("TransitionTimer")
-	_transition_timer.text = "Time Remaining: N/A"
+	
+## Called when this box should switch to green.
+func switch_to_green():
+	_switch_to(1)
 
-	red_state.state_entered.connect(_on_red_state_entered)
-	green_state.state_entered.connect(_on_green_state_entered)
-	blue_state.state_entered.connect(_on_blue_state_entered)
-
-	compound_state.state_processing.connect(_on_state_processing)
-	red_state.transition_pending.connect(_on_transition_pending)
-	green_state.transition_pending.connect(_on_transition_pending)
-	blue_state.transition_pending.connect(_on_transition_pending)
-
-
-func _on_red_state_entered() -> void:
-	color = colors[0]
-	_update_state(color)
-
-func _on_green_state_entered() -> void:
-	color = colors[1]
-	_update_state(color)
-
-func _on_blue_state_entered() -> void:
-	color = colors[2]
-	_update_state(color)
-
-func _update_state(new_color: Color, time_in_state: float = 0.0, transition_time_remaining: float = 0.0) -> void:
-	_time_in_state = time_in_state
-	color = new_color
-	if transition_time_remaining > 0.0:
-		_transition_timer.text = "Time Remaining: %.1f" % transition_time_remaining
-	else:
-		_transition_timer.text = "Time Remaining: N/A"
-	find_child("ColorRect").color = color
+## Called when this box should switch to blue.
+func switch_to_blue():
+	_switch_to(2)
 
 
-func _on_state_processing(delta: float) -> void:
+func _switch_to(index: int):
+	_current_color_index = index
+	_color_rect.color = COLORS[index]
+	_transition_time_label.text = ""
+	_time_in_state = 0
+
+## Called every frame while a color change is pending
+func show_pending(_initial_delay: float, remaining_delay: float):
+	_transition_time_label.text = "Time Remaining: %.1f" % remaining_delay
+
+
+func _physics_process(delta):
 	_time_in_state += delta
-	_timer.text = "Time in State: %.1f" % _time_in_state
-
-func _on_transition_pending(_initial_delay: float, remaining_delay: float) -> void:
-	_transition_timer.text = "Time Remaining: %.1f" % remaining_delay
-
-
-func _on_button_pressed() -> void:
-	chart.send_event("%s-click" % box_id)
+	_time_in_state_label.text = "Time in State: %.1f" % _time_in_state
 
 
 func save_state() -> SerializedColorBox:
-	var serialized_color_box: SerializedColorBox = SerializedColorBox.new()
-	serialized_color_box.box_id = box_id
-	serialized_color_box.color = color
-	serialized_color_box.time_in_state = _time_in_state
-	print("Saving state for %s" % box_id)
-	print(JSON.stringify(serialized_color_box, "\t"))
-	return serialized_color_box
+	var result: SerializedColorBox = SerializedColorBox.new()
+	result.box_id = box_id
+	result.color_index = _current_color_index
+	result.time_in_state = _time_in_state
+	return result
 
 
 func load_state(state: SerializedColorBox) -> void:
-	print("Loading state for %s" % box_id)
-	print(JSON.stringify(state, "\t"))
-
-	color = state.color
+	_switch_to(state.color_index)
 	_time_in_state = state.time_in_state
-	_update_state(color, _time_in_state)
--- godot_state_charts_examples/save_and_load/color_box/serialized_color_box.gd
+++ godot_state_charts_examples/save_and_load/color_box/serialized_color_box.gd
@@ -1,13 +1,13 @@
 class_name SerializedColorBox extends Resource
 
 @export var box_id: String = ""
-@export var color: Color = Color.GHOST_WHITE
+@export var color_index: int = 0
 @export var time_in_state: float = 0.0
 
 
-func debug_string() -> String:
+func _to_string() -> String:
 	return """SerializedColorBox(
 		box_id: %s
-		color: %s
+		color_index: %s
 		time_in_state: %s
-	)""" % [box_id, color, time_in_state]
+	)""" % [box_id, color_index, time_in_state]
